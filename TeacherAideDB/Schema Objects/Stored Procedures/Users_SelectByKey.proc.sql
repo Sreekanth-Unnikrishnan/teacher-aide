6f2c89524a72c152224dcec510fdf55465550d28
sreesmru <sreesmru@users.noreply.github.com>
2009-04-26 20:57:29 +0000

    Added Users_Insert and Users_SelectByKey procedures

---
+++ TeacherAideDB/Schema Objects/Stored Procedures/Users_SelectByKey.proc.sql
@@ -0,0 +1,11 @@
+﻿CREATE PROCEDURE dbo.Users_SelectByKey
+  @ta_user_id INT
+AS
+SELECT ta_user_id,
+       network_user_id,
+       last_name,
+       first_name,
+       middle_name,
+       nickname
+  FROM dbo.Users
+ WHERE ta_user_id = @ta_user_id
