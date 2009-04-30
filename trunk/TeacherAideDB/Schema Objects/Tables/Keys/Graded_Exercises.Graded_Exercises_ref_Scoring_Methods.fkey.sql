301c9c112611e4d1386c069cab848eb2d5a4a3be
akaitatsu <akaitatsu@bfd05f82-30f0-11de-bd8d-4b29057232bf>
2009-04-30 18:57:36 +0000

    Some files were missed in the last commit.

---
+++ trunk/TeacherAideDB/Schema Objects/Tables/Keys/Graded_Exercises.Graded_Exercises_ref_Scoring_Methods.fkey.sql
@@ -0,0 +1,5 @@
+﻿   ALTER TABLE dbo.Graded_Exercises
+ADD CONSTRAINT Graded_Exercises$ref$Scoring_Methods
+   FOREIGN KEY (scoring_method_code)
+    REFERENCES dbo.Scoring_Methods (scoring_method_code)	
+
---
+++ trunk/TeacherAideDB/Schema Objects/Tables/Keys/Graded_Exercises_course_id_assignment_id_scoring_method_code_Key.ukey.sql
@@ -0,0 +1,3 @@
+﻿   ALTER TABLE dbo.Graded_Exercises
+ADD CONSTRAINT Graded_Exercises$course_id$assignment_id$scoring_method_code$Key
+        UNIQUE (course_id, exercise_seq, scoring_method_code)
